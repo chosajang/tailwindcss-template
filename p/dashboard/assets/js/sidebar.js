106227306521cf450eb486f5bb353dc68e0f180a
jonny <enjoy.hyun@gmail.com>
2022-02-22 00:27:35 +0900

    사이드바 스타일 변경

--- p/dashboard/assets/js/sidebar.js
+++ p/dashboard/assets/js/sidebar.js
@@ -1,24 +1,17 @@
 const asideHTML = `
-<!--
-<div class="flex flex-none items-center h-12 bg-gray-800">
-  <a href="#" class="flex m-auto text-base text-gray-200 items-center font-semibold tracking-widest uppercase focus:outline-none focus:shadow-outline">
-    <img class="w-6 h-6 mr-2" src="${documentRoot}assets/images/logo.png" alt="Site Logo" />
-    Web Console
-  </a>
-</div> -->
-<!--// Left Menu : ST -->
+<!-- Left Menu : ST -->
 <div>
   <ul class="select-none mt-4" id="menu-ext">
-<!--    <li class="pt-4 py-2 pl-4"><p class="text-xs font-bold text-gray-300">기본 메뉴</p></li>-->
-    <a href="${documentRoot}"><li class="py-3 pl-4 text-xs cursor-pointer hover:text-slate-900 hover:bg-slate-300 border-r-4 border-transparent hover:border-blue-500 "><i class="fas fa-columns w-4 mr-2"></i>대시보드</li></a>
-    <li class="py-3 pl-4 text-xs cursor-pointer hover:text-slate-900 hover:bg-slate-300 border-r-4 border-transparent hover:border-blue-500 "><i class="fas fa-users w-4 mr-2"></i>계정관리</li>
+    <a href="${documentRoot}"><li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500 "><i class="fas fa-columns w-4 mr-2"></i>대시보드</li></a>
+    <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500 "><i class="fas fa-users w-4 mr-2"></i>계정관리</li>
     <ul>
-      <li class="py-2 pl-6 hover:text-slate-900 cursor-pointer hover:bg-slate-300">test</li>
-      <li class="py-2 pl-6 hover:text-slate-900 cursor-pointer hover:bg-slate-300">test</li>
-      <li class="py-2 pl-6 hover:text-slate-900 cursor-pointer hover:bg-slate-300">test</li>
+      <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
+      <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
+      <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
     </ul>
   </ul>
-</div><!--// Left Menu : ED -->
+</div>
+<!-- Left Menu : ED -->
 `;
 
 /**
@@ -27,10 +20,10 @@ const asideHTML = `
 document.addEventListener("DOMContentLoaded", function(){
   let asideObj = document.getElementById('aside-menu');
   asideObj.classList.add('fixed','top-0','left-0','z-40','w-52','pt-12','h-screen');
-  asideObj.classList.add('bg-slate-100','text-slate-700','text-sm');
+  asideObj.classList.add('bg-white','text-slate-700','text-sm');
   asideObj.classList.add('items-center','overflow-y-auto','flex-row','flex-no-wrap','overflow-hidden');
   asideObj.classList.add('transform','-translate-x-52','md:translate-x-0','ease-out','duration-200');
-  asideObj.classList.add('border-r-[1px]','border-slate-300');
+  asideObj.classList.add('border-r-[1px]','border-slate-200');
   asideObj.innerHTML = asideHTML;
   
   // 메뉴 펼침/닫힘 이벤트
