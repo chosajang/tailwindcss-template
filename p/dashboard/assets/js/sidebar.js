429652d9702bc13645425fc139b8aaf7ed5ccd80
jonny <enjoy.hyun@gmail.com>
2022-02-22 22:14:13 +0900

    사이드 : 팝업 메뉴 추가

--- p/dashboard/assets/js/sidebar.js
+++ p/dashboard/assets/js/sidebar.js
@@ -1,15 +1,24 @@
 const asideHTML = `
 <!-- Left Menu : ST -->
-<div>
+<div class="overflow-y-auto overflow-x-hidden">
+  <!-- uncollapse -->
   <ul class="select-none mt-4" id="menu-ext">
-    <a href="${documentRoot}"><li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500 "><i class="fas fa-columns w-4 mr-2"></i>대시보드</li></a>
-    <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500 "><i class="fas fa-users w-4 mr-2"></i>계정관리</li>
+    <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500 "><i class="fas fa-columns w-4 mr-2"></i>대시보드</li>
+    <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500">
+      <span><i class="fas fa-users w-4 mr-2"></i>계정관리</span>
+      <!-- 하위 메뉴 -->
+      <div class="fixed left-52 top-24 w-52 h-10 p-2 bg-white border border-slate-200 shadow-lg">
+        TEST
+      </div>
+    </li>
     <ul>
       <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
       <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
       <li class="py-2 pl-6 cursor-pointer hover:bg-sky-50 hover:text-sky-600">test</li>
     </ul>
   </ul>
+  <!-- collapse -->
+  <div class="absolute left-48 z-20 bg-slate-300 rounded p-3">fixed TEST</div>
 </div>
 <!-- Left Menu : ED -->
 `;
@@ -19,9 +28,9 @@ const asideHTML = `
  */
 document.addEventListener("DOMContentLoaded", function(){
   let asideObj = document.getElementById('aside-menu');
-  asideObj.classList.add('fixed','top-0','left-0','z-40','w-52','pt-12','h-screen');
+  asideObj.classList.add('fixed','top-0','left-0','z-10','w-52','h-screen','pt-12');
   asideObj.classList.add('bg-white','text-slate-700','text-sm');
-  asideObj.classList.add('items-center','overflow-y-auto','flex-row','flex-no-wrap','overflow-hidden');
+  asideObj.classList.add('items-center','flex-row','flex-no-wrap',);
   asideObj.classList.add('transform','-translate-x-52','md:translate-x-0','ease-out','duration-200');
   asideObj.classList.add('border-r-[1px]','border-slate-200');
   asideObj.innerHTML = asideHTML;
