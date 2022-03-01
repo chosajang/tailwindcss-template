b1e34d549a09a92c8b1198d2470d78588fdddea4
jonny <enjoy.hyun@gmail.com>
2022-03-01 19:31:24 +0900

    사이드바 확대/축소 이벤트

--- p/dashboard/assets/js/sidebar.js
+++ p/dashboard/assets/js/sidebar.js
@@ -1,9 +1,9 @@
 const asideHTML = `
 <!-- Left Menu : ST -->
-<div class="min-h-max overflow-x-hidden">
-  <ul class="select-none mt-4" id="sidebar">
+<div id="sidebar-wrapper" class="min-h-max overflow-x-hidden">
+  <ul class="select-none mt-4">
     <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500">
-      <span><i class="fas fa-columns w-4 mr-2"></i>대시보드</span>
+      <i class="fas fa-columns w-4 mr-2"></i><span>대시보드</span>
       <!-- 하위 메뉴 -->
       <div class="hidden fixed left-52 top-12 w-40 m-1 text-slate-700 text-left bg-white border border-slate-300 rounded-lg shadow transition ease-in-out duration-200">
         <div class="cursor-default py-2 pl-4 bg-sky-50 text-slate-800 font-bold">대시보드</div>
@@ -15,7 +15,7 @@ const asideHTML = `
       </div>
     </li>
     <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500">
-      <span><i class="fa-solid fa-server w-4 mr-2"></i>자원관리</span>
+      <i class="fa-solid fa-server w-4 mr-2"></i><span>자원관리</span>
       <!-- 하위 메뉴 -->
       <div class="hidden fixed left-52 top-16 w-40 m-1 text-slate-700 text-left bg-white border border-slate-300 rounded-lg shadow transition ease-in-out duration-200">
         <div class="cursor-default py-2 pl-4 bg-sky-50 text-slate-800 font-bold">자원 관리</div>
@@ -28,7 +28,7 @@ const asideHTML = `
       </div>
     </li>
     <li class="py-3 pl-4 text-xs cursor-pointer hover:text-sky-600 hover:bg-sky-50 border-r-4 border-transparent hover:border-sky-500">
-      <span><i class="fa-solid fa-cubes w-4 mr-2"></i>컴포넌트</span>
+      <i class="fa-solid fa-cubes w-4 mr-2"></i><span>컴포넌트</span>
       <!-- 하위 메뉴 -->
       <div class="hidden fixed left-52 top-28 w-40 m-1 text-slate-700 text-left bg-white border border-slate-300 rounded-lg shadow transition ease-in-out duration-200">
         <div class="cursor-default py-2 pl-4 bg-sky-50 text-slate-800 font-bold">컴포넌트</div>
@@ -40,6 +40,11 @@ const asideHTML = `
       </div>
     </li>
   </ul>
+  <div class="absolute w-full bottom-0 border-t">
+    <div id="collapse" class="pl-4 py-3 border-slate-200 cursor-pointer hover:bg-sky-50 hover:text-sky-600 border-r-4 border-transparent hover:border-sky-500">
+      <i class="fa-solid fa-angles-left w-4 mr-2"></i><span>Collapse Sidebar<span>
+    </div>
+  </div>
 </div>
 <!-- Left Menu : ED -->
 `;
@@ -48,17 +53,15 @@ const asideHTML = `
  * DOM 트리 완성 후 실행
  */
 document.addEventListener("DOMContentLoaded", function(){
-  let asideObj = document.getElementById('aside-menu');
-  asideObj.classList.add('fixed','top-0','left-0','z-10','w-52','h-screen','pt-12');
-  asideObj.classList.add('bg-white','text-slate-700','text-sm');
-  asideObj.classList.add('items-center','flex-row','flex-no-wrap',);
-  asideObj.classList.add('transform','-translate-x-52','md:translate-x-0','ease-out','duration-200');
-  asideObj.classList.add('border-r-[1px]','border-slate-200');
+  const asideObj = document.getElementById("aside-menu");
+  asideObj.classList.add("fixed","top-0","left-0","z-10","w-52","h-screen","pt-12");
+  asideObj.classList.add("bg-white","text-slate-700","text-sm");
+  asideObj.classList.add("items-center","flex-row","flex-no-wrap",);
+  asideObj.classList.add("transform","-translate-x-52","md:translate-x-0","ease-out","duration-200");
+  asideObj.classList.add("border-r-[1px]","border-slate-200");
   asideObj.innerHTML = asideHTML;
 
-  const liList = document.querySelectorAll("#sidebar > li");
-  const sidebarObj = document.getElementById("sidebar").parentElement;
-
+  const liList = document.querySelectorAll("#sidebar-wrapper > ul > li");
 
   let parentMenuFocus = false;
   let childMenuFocus = false;
@@ -66,35 +69,35 @@ document.addEventListener("DOMContentLoaded", function(){
   let prevChildObj = null;
   // 메뉴 마우스 오버 시, 하위 메뉴 팝업
   liList.forEach( liObj => {
-    liObj.addEventListener('mouseenter', () => {
-      parentChildObj = liObj.querySelector('div');
+    liObj.addEventListener("mouseenter", () => {
+      parentChildObj = liObj.querySelector("div");
       parentMenuFocus = true;
 
       // 이전 하위 메뉴 객체 체크
       if(prevChildObj !== null) {
-        prevChildObj.classList.add('hidden');
+        prevChildObj.classList.add("hidden");
       }
 
       // 하위 요소 체크
       if(parentChildObj !== null ){
         // 하위 메뉴가 있는 경우 보이기
-        parentChildObj.classList.remove('hidden');
+        parentChildObj.classList.remove("hidden");
 
         // 1차 메뉴 마우스 아웃 이벤트
-        liObj.addEventListener('mouseleave',() => {
+        liObj.addEventListener("mouseleave",() => {
           parentMenuFocus = false;
           // 하위 메뉴 접근 판단을 위해 딜레이 타임 후, 상태 체크
           setTimeout(menuEvent, 200);
         });
 
         // 2차 메뉴 마우스 오버 이벤트
-        parentChildObj.addEventListener('mouseenter', () => {
+        parentChildObj.addEventListener("mouseenter", () => {
           childMenuFocus = true;
         });
 
         // 2차 메뉴에서 마우스 아웃 이벤트
-        parentChildObj.addEventListener('mouseleave', () => {
-          parentChildObj.classList.add('hidden');
+        parentChildObj.addEventListener("mouseleave", () => {
+          parentChildObj.classList.add("hidden");
           childMenuFocus = false;
         });
 
@@ -105,13 +108,56 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   });
 
+  // 2차 메뉴 가리기
   let menuEvent = () => {
     if(prevChildObj !== null && parentMenuFocus === false && childMenuFocus === false){
-      prevChildObj.classList.add('hidden');
+      prevChildObj.classList.add("hidden");
     }
   };
 
   // 주소기반 메뉴 포커스
-  const currentUrl = window.location.href;
-  console.log(currentUrl);
+  // const currentUrl = window.location.href;
+  // console.log(currentUrl);
+
+  // Collapse Sidebar Event
+  const contentWrapperObj = document.getElementById("content-wrapper");
+  const collapseObj = document.getElementById("collapse");
+  const menuTextListObj = document.querySelectorAll("#sidebar-wrapper > ul > li > span,#collapse > span");
+  const sidebarChildListObj = document.querySelectorAll("#sidebar-wrapper > ul > li > div");
+  const collapseIconObj = document.querySelector("#sidebar-wrapper > div > div > i");
+
+  let collapseCheck = false;
+  collapseObj.addEventListener("click", () => {
+    // Collapse Icon 변경
+    collapseIconObj.classList.toggle("fa-angles-left");
+    collapseIconObj.classList.toggle("fa-angles-right");
+    // 컨텐츠 크기 조절
+    contentWrapperObj.classList.toggle("md:ml-52");
+    contentWrapperObj.classList.toggle("md:ml-12");
+    // 사이드바 크기 조절
+    asideObj.classList.toggle("w-52");
+    asideObj.classList.toggle("w-12");
+    // 사이드바 2차 메뉴 위치 조절
+    sidebarChildListObj.forEach(childMenuObj => {
+      childMenuObj.classList.toggle("left-52");
+      childMenuObj.classList.toggle("left-12");
+    })
+
+    // collapse
+    if(collapseCheck) {
+      setTimeout(menuTextToggle,200);
+      collapseCheck = false;
+    } else {
+      menuTextToggle();
+      collapseCheck = true;
+    }
+  });
+
+  let menuTextToggle = () => {
+    menuTextListObj.forEach( menuTextObj => {
+      // 텍스트 가리기
+      menuTextObj.classList.toggle("hidden");
+    });
+  }
+
 });
